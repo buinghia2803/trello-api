e867157c8763ee0b6f81b2df45bab57d5c38be93
truongdv <truongdv@relipasoft.com>
2024-03-20 21:36:24 +0700

    commit 20/03/2024 cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@
 
 // Những domain đc phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173',
+  // 'http://localhost:5173', // ko cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
   // ...vv ví dụ sau này sẽ deploy lên domain chính thức ...vv
 ]
 
